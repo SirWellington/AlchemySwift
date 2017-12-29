695bbbc7a04b2e551d038195301ba6d3600b32a9
Wellington Moreno <wellington@censio.love>
2017-12-29 07:49:07 -0800

    Adds first couple of extensions

---
+++ AlchemySwift/AlchemySwift/Extensions/Array+Alchemy.swift
@@ -0,0 +1,132 @@
+//
+//  Array+Alchemy.swift
+//  AlchemySwift
+//
+//  Created by Wellington Moreno on 12/29/2017.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+import Foundation
+
+
+//======================================
+//MARK: Accessing and Inserting Elements
+//======================================
+
+public extension Array
+{
+    var notEmpty: Bool { return !isEmpty }
+
+    var anyElement: Element?
+    {
+        if isEmpty
+        {
+            return nil
+        }
+
+        guard let index = (0..<count).anyElement, isValidIndex(index) else
+        {
+            return nil
+        }
+
+        return self[index]
+    }
+
+    func isValidIndex(_ index: Int) -> Bool
+    {
+        return index.isValidIndex(toArray: self)
+    }
+
+    subscript (safe index: Index) -> Element?
+    {
+        return self.isValidIndex(index) ? self[index] : nil
+    }
+
+    /**
+         Adds an element to the beginning of an Array.
+     */
+    mutating func prepend(_ element: Element)
+    {
+        insert(element, at: 0)
+    }
+}
+
+public extension CountableRange where Element == Int
+{
+    var anyElement: Element?
+    {
+        let index = Int.randomFrom(minInclusive: self.lowerBound, maxExclusive: self.upperBound)
+
+        return index
+    }
+}
+
+//======================================
+//MARK: Shuffling
+//======================================
+public extension Array
+{
+
+    func shuffled() -> Array<Element>
+    {
+
+        guard count >= 2
+        else
+        {
+            return self
+        }
+
+        var mutable = Array(self)
+
+        for (index, _) in enumerated()
+        {
+            let randomIndex = Int.randomFrom(minInclusive: 0, maxExclusive: count)
+            mutable.swapAt(index, randomIndex)
+        }
+
+        return mutable
+    }
+}
+
+//======================================
+//MARK: Equality
+//======================================
+public extension Array where  Element: Equatable
+{
+    static func ==(lhs: Array<Element>, rhs: Array<Element>) -> Bool
+    {
+        guard lhs.notEmpty, rhs.notEmpty else { return true }
+        guard lhs.count == rhs.count else { return false }
+
+        let size = lhs.count
+
+        for index in 0..<size
+        {
+            let leftValue = lhs[index]
+            let rightValue = rhs[index]
+
+            guard leftValue == rightValue else { return false }
+        }
+
+        return true
+    }
+}
+
+//======================================
+//MARK: Sequence Operations
+//======================================
+public extension Sequence where Element: Equatable
+{
+    func countWhere(_ predicate: (Element) -> Bool) -> Int
+    {
+        return filter(predicate).count
+    }
+
+    func repeatBlock(_ block: () -> ())
+    {
+        for _ in self
+        {
+            block()
+        }
+    }
+}
---
+++ AlchemySwift/AlchemySwift/Extensions/Int+Alchemy.swift
@@ -0,0 +1,87 @@
+//
+//  Int+Alchemy.swift
+//  AlchemySwift
+//
+//  Created by Wellington Moreno on 12/29/2017.
+//  Copyright © 2017 Wellington Moreno. All rights reserved.
+//
+
+import Foundation
+
+//=====================================
+//MARK: Basics
+//=====================================
+extension Int
+{
+
+    func isValidIndex<T>(toArray array: Array<T>) -> Bool
+    {
+
+        if self < 0
+        {
+            return false
+        }
+
+        return self < array.count
+    }
+
+    static func randomFrom(minInclusive: Int, maxExclusive: Int) -> Int
+    {
+
+        guard maxExclusive > minInclusive
+        else
+        {
+            return maxExclusive
+        }
+
+        let distance = maxExclusive - minInclusive
+
+        let randomValue = Int(arc4random_uniform(UInt32(distance)))
+        let result = randomValue + minInclusive
+
+        return result
+    }
+
+    var isEven: Bool
+    {
+        return self % 2 == 0
+    }
+
+    var isOdd: Bool
+    {
+        return !isEven
+    }
+
+    static var random: Int
+    {
+        return randomFrom(minInclusive: 0, maxExclusive: 10_000)
+    }
+
+    func repeatBlock(_ block: () -> ())
+    {
+        guard self > 0 else { return }
+
+        (1...self).repeatBlock(block)
+    }
+}
+
+
+//=====================================
+//MARK: Conversions
+//=====================================
+
+public extension Int
+{
+    var asDouble: Double { return Double(self) }
+    var doubleValue: Double { return asDouble }
+    var uInt32: UInt32 { return UInt32(self) }
+}
+
+public extension UInt32
+{
+
+    var intValue: Int
+    {
+        return Int(self)
+    }
+}
