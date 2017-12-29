4a92e8e81e217a0b160404b844936a19326b69e7
Wellington Moreno <wellington@censio.love>
2017-12-29 07:41:29 -0800

    Initial commit 👨🏽‍💻

---
+++ AlchemySwift/AlchemySwift/AlchemySwift.h
@@ -0,0 +1,19 @@
+//
+//  AlchemySwift.h
+//  AlchemySwift
+//
+//  Created by Wellington Moreno on 12/29/17.
+//  Copyright © 2017 Sir Wellington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlchemySwift.
+FOUNDATION_EXPORT double AlchemySwiftVersionNumber;
+
+//! Project version string for AlchemySwift.
+FOUNDATION_EXPORT const unsigned char AlchemySwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlchemySwift/PublicHeader.h>
+
+
