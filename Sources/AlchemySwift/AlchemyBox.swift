f7c3c89bbcf1efa0fb51f07560239c87b1410b8b
Wellington Moreno <wellington@blacksource.tech>
2025-04-14 10:17:54 -0700

    updates

--- Sources/AlchemySwift/AlchemyBox.swift
+++ Sources/AlchemySwift/AlchemyBox.swift
@@ -3,99 +3,89 @@
 //  AlchemySwift
 //
 //  Created by Wellington Moreno on 7/28/19.
-//  Copyright © 2019 Wellington Moreno. All rights reserved.
+//  Copyright © 2025 Wellington Moreno. All rights reserved.
 //
 
 import Foundation
 
 //=========================================
-//MARK: STRONG BOX
+// MARK: STRONG BOX
 //=========================================
 
-/**
-    Use as a container to hold a strong to another object.
- */
-public class AlchemyStrongBox<T: Any>
-{
+/// Use as a container to hold a strong reference to another object.
+public class AlchemyStrongBox<T: Any> {
+
     public var item: T
-    
-    public init(item: T)
-    {
+
+    public init(item: T) {
         self.item = item
     }
 }
 
-public extension Anything
-{
-    func box() -> AlchemyStrongBox<Self>
-    {
+public extension Anything {
+
+    func box() -> AlchemyStrongBox<Self> {
         return AlchemyStrongBox(item: self)
     }
-    
 }
 
-extension AlchemyStrongBox: Equatable where T: Equatable
-{
-    public static func ==(lhs: AlchemyStrongBox<T>, rhs: AlchemyStrongBox<T>) -> Bool
-    {
+extension AlchemyStrongBox: Equatable where T: Equatable {
+
+    public static func == (
+        lhs: AlchemyStrongBox<T>,
+        rhs: AlchemyStrongBox<T>
+    ) -> Bool {
         return lhs.item == rhs.item
     }
 }
 
-extension AlchemyStrongBox: Hashable where T: Hashable
-{
-    public func hash(into hasher: inout Hasher)
-    {
+extension AlchemyStrongBox: Hashable where T: Hashable {
+
+    public func hash(into hasher: inout Hasher) {
         hasher.combine(self.item)
     }
 }
 
-extension AlchemyStrongBox: Anything { }
+extension AlchemyStrongBox: Anything {}
 
 
 //=========================================
-//MARK: WEAK BOX
+// MARK: WEAK BOX
 //=========================================
 
-/**
- Use as a container to hold a weak reference to another object.
- */
-public class AlchemyWeakBox<T: AnyObject>
-{
+/// Use as a container to hold a weak reference to another object.
+public class AlchemyWeakBox<T: AnyObject> {
+
     public weak var item: T?
-    
-    public init(item: T)
-    {
+
+    public init(item: T) {
         self.item = item
     }
 }
 
-public extension Anything where Self: AnyObject
-{
-    func weakBox() -> AlchemyWeakBox<Self>
-    {
+public extension Anything where Self: AnyObject {
+
+    func weakBox() -> AlchemyWeakBox<Self> {
         return AlchemyWeakBox(item: self)
     }
 }
 
-extension AlchemyWeakBox: Equatable where T: Equatable
-{
-    public static func ==(lhs: AlchemyWeakBox<T>, rhs: AlchemyWeakBox<T>) -> Bool
-    {
+extension AlchemyWeakBox: Equatable where T: Equatable {
+
+    public static func == (
+        lhs: AlchemyWeakBox<T>,
+        rhs: AlchemyWeakBox<T>
+    ) -> Bool {
         return lhs.item == rhs.item
     }
 }
 
-extension AlchemyWeakBox: Hashable where T: Hashable
-{
-    public func hash(into hasher: inout Hasher)
-    {
-        if let item = self.item
-        {
+extension AlchemyWeakBox: Hashable where T: Hashable {
+
+    public func hash(into hasher: inout Hasher) {
+        if let item = self.item {
             hasher.combine(item)
-        }
-        else
-        {
+        } else {
             hasher.combine(0)
         }
     }
