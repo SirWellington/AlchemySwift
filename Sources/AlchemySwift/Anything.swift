9d23cad744471f695104219a00ae0fdac164060a
Wellington Moreno <wellington@blacksource.tech>
2025-04-14 13:36:04 -0700

    copyright updates

--- Sources/AlchemySwift/Anything.swift
+++ Sources/AlchemySwift/Anything.swift
@@ -3,7 +3,7 @@
 //  AlchemySwift
 //
 //  Created by Wellington Moreno on 3/20/18.
-//  Copyright © 2019 Wellington Moreno. All rights reserved.
+//  Copyright © 2025 Wellington Moreno. All rights reserved.
 //
 
 import Foundation
--- Sources/AlchemySwift/Functions.swift
+++ Sources/AlchemySwift/Functions.swift
@@ -3,7 +3,7 @@
 //  AlchemySwift
 //
 //  Created by Wellington Moreno on 9/9/18.
-//  Copyright © 2019 Wellington Moreno. All rights reserved.
+//  Copyright © 2025 Wellington Moreno. All rights reserved.
 //
 
 import Foundation
