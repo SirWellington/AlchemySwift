7970d3e5ca319565f6863ae1b93e643cbe385358
Wellington Moreno <wellington@blacksource.tech>
2025-04-14 15:58:40 -0700

    Update DatesPlusAlchemyTests.swift

--- Tests/AlchemySwiftTests/Extensions/DatesPlusAlchemyTests.swift
+++ Tests/AlchemySwiftTests/Extensions/DatesPlusAlchemyTests.swift
@@ -119,7 +119,10 @@ class DatesPlusAlchemyTests: AlchemyTest {
 
     func testYearsAgo() {
         repeatTest {
-            let yearsAgo = AlchemyGenerator.integer(fromInclusive: 0, toInclusive: 100)
+            let yearsAgo = AlchemyGenerator.integer(
+                fromInclusive: 0,
+                toInclusive: 100
+            )
             let now = Date()
             let date = calendar.date(byAdding: .year, value: -yearsAgo, to: now)!
 
@@ -130,7 +133,10 @@ class DatesPlusAlchemyTests: AlchemyTest {
 
     func testYearsAgoWithFutureDate() {
         repeatTest {
-            let yearsAhead = AlchemyGenerator.integer(fromInclusive: 0, toInclusive: 100)
+            let yearsAhead = AlchemyGenerator.integer(
+                fromInclusive: 0,
+                toInclusive: 100
+            )
             let now = Date()
             var date = calendar.date(byAdding: .year, value: yearsAhead, to: now)!
             // Adjust for a couple of millis of computation time
@@ -143,7 +149,10 @@ class DatesPlusAlchemyTests: AlchemyTest {
 
     func testMonthsAgo() {
         repeatTest {
-            let monthsAgo = AlchemyGenerator.integer(fromInclusive: 0, toInclusive: 200)
+            let monthsAgo = AlchemyGenerator.integer(
+                fromInclusive: 0,
+                toInclusive: 200
+            )
             let now = Date()
             let date = calendar.date(byAdding: .month, value: -monthsAgo, to: now)!
 
@@ -154,7 +163,10 @@ class DatesPlusAlchemyTests: AlchemyTest {
 
     func testMonthsAgoWithFutureDate() {
         repeatTest {
-            let monthsAhead = AlchemyGenerator.integer(fromInclusive: 0, toInclusive: 200)
+            let monthsAhead = AlchemyGenerator.integer(
+                fromInclusive: 0,
+                toInclusive: 200
+            )
             let now = Date()
             var date = calendar.date(byAdding: .month, value: monthsAhead, to: now)!
             // Adjust for computation time
